b0eda8670fda8371df7487bbabb189bc6699ed1d
ceald1 <ceald@ceald.cc>
2025-10-26 16:16:08 -0700

    added more functionality to the db module

---
+++ modules/alerts/alerts.go
@@ -0,0 +1,10 @@
+package alerts
+import (
+	Framework "github.com/Ceald1/purrimeter/modules/framework"
+	YAML "github.com/goccy/go-yaml"
+)
+
+
+func main(
+	
+)
--- modules/framework/dbframework.go
+++ modules/framework/dbframework.go
@@ -2,7 +2,11 @@ package framework
 
 import (
 	"context"
+	"crypto/sha256"
+	"encoding/binary"
 	"encoding/json"
+	"fmt"
+	"time"
 
 	manticoresearch "github.com/manticoresoftware/manticoresearch-go"
 )
@@ -101,5 +105,64 @@ func (s *DBFramework) MatchLargerThanJSON(match_value string, matchKey string, l
 	defer httpResp.Body.Close()
 	err = json.NewDecoder(httpResp.Body).Decode(&result)
 	return
+}
+
+
+// copied from DB package
+func generateLogID(data string, ts int64) uint64 {  
+    h := sha256.New()  
+    h.Write([]byte(data))  
+    binary.Write(h, binary.BigEndian, ts)  
+    return binary.BigEndian.Uint64(h.Sum(nil)[:8])  
+}
+
+func convertTimestampToInt64(timestampStr string) (int64, error) {  
+    // Parse the RFC3339 timestamp  
+    t, err := time.Parse(time.RFC3339Nano, timestampStr)  
+    if err != nil {  
+        return 0, err  
+    }  
+      
+    // Convert to Unix timestamp (seconds since epoch)  
+    return t.Unix(), nil  
+}  
+
+func manti_SubmitLogRaw(client *manticoresearch.APIClient, log map[string]interface{}, index string) error {  
+    // Extract timestamp
+	nextID := time.Now().UnixNano()
+    timestampStr, ok := log["timestamp"].(string)  
+    if !ok {  
+        return fmt.Errorf("timestamp is missing or not a string")  
+    }  
+  
+    timestamp, err := convertTimestampToInt64(timestampStr)  
+    if err != nil {  
+        return err  
+    }  
+  
+    // Serialize the original log for raw_text  
+    stringifiedLog, err := json.Marshal(log)  
+    if err != nil {  
+        return err  
+    }  
+  
+    // Create the document for Manticore  
+    formatted_log := make(map[string]interface{})  
+    formatted_log["ts"] = timestamp  
+    formatted_log["raw_text"] = string(stringifiedLog)
+    // JSON field should be the actual map, not stringified
+    formatted_log["data"] = log
+	formatted_log["log_number"] = nextID
+  
+    insertReq := manticoresearch.NewInsertDocumentRequest(index, formatted_log)  
+    insertReq.SetId(generateLogID(string(stringifiedLog), timestamp))  
+  
+    _, _, err = client.IndexAPI.Insert(ctx).InsertDocumentRequest(*insertReq).Execute()  
+    return err  
+}
+
+func (s *DBFramework) AddLog(log map[string]interface{}, index string) (err error) {
+	err = manti_SubmitLogRaw(s.MantiClient, log, index)
+	return err
 }
 
