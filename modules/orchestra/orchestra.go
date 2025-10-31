b526c83141c80464f1e1f40bd29eecd0e4e003f1
ceald1 <ceald@ceald.cc>
2025-10-31 01:50:04 -0700

    conductor

--- modules/orchestra/orchestra.go
+++ modules/orchestra/orchestra.go
@@ -5,22 +5,16 @@ import (
 	"os"
 
 	Framework "github.com/Ceald1/purrimeter/modules/framework"
-	"github.com/valkey-io/valkey-go"
+	"encoding/json"
+	"net/http"
+	"time"
 )
 
 // orchestra microservice/module this module is for orchestrating where alerts go. For registering modules/integrations check the conductor component.
 
 func main(){
-	valkey_host := os.Getenv("VALKEY_HOST")
-	if valkey_host == "" {
-		valkey_host = "valkey"
-	}
-	pass := os.Getenv("REDIS_PASS")
-
-	valk, err := valkey.NewClient(valkey.ClientOption{InitAddress: []string{fmt.Sprintf("%s:6379", valkey_host)}, Password: pass})
-	if err != nil {
-		panic(err)
-	}
+	conductor_host := os.Getenv("CONDUCTOR_HOST")
+	
 	
 
 
